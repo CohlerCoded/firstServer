85b1530e169e6576fa98a6cbdef976f890da41c6
cohlerCoded <jeffcohler@cohlercoded.com>
2021-02-15 15:30:56 -0800

    moved replaceTemp to module for index.js to import

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemp = require("./modules/replaceTemplate.js");
 
 ////////////////////FILES//////////////////////////////////////////
 
@@ -28,20 +29,6 @@ const url = require("url");
 // console.log("Will read file!");
 
 ////////////////////SERVER//////////////////////////////////////////
-const replaceTemp = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-  return output;
-};
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -65,7 +52,6 @@ const server = http.createServer((req, res) => {
     const cardsHtml = dataObj
       .map((card) => replaceTemp(tempCard, card))
       .join("");
-    console.log(cardsHtml);
     const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
     res.end(output);
     //Product Page
