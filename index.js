8b40c822a8ad90554aaf31b2f8245c205d31fc07
cohlerCoded <jeffcohler@cohlercoded.com>
2021-02-16 10:38:27 -0800

    added scripts and dependencies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 const replaceTemp = require("./modules/replaceTemplate.js");
 
 ////////////////////FILES//////////////////////////////////////////
@@ -43,7 +44,8 @@ const tempCard = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
-
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
   //Overview Page
